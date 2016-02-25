cd1304472445df56290e2dda8dd72abf6331ad65
Wyatt Kirby <kirby.wa@gmail.com>
2016-02-25 12:50:01 -0800

    Fix constant references

--- lib/jekyll/tidy.rb
+++ lib/jekyll/tidy.rb
@@ -5,16 +5,16 @@ require "sanitize"
 module Jekyll
     module Tidy
         def output_clean(output)
-            output = Sanitize.document(post.output, Sanitize::Config::RELAXED)
+            output = Sanitize.document(post.output, ::Sanitize::Config::RELAXED)
             return HtmlBeautifier.beautify(output)
         end
 
         Jekyll::Hooks.register :posts, :post_render do |post|
-            post.output = output_clean(post.output)
+            post.output = Jekyll::Tidy::output_clean(post.output)
         end
 
         Jekyll::Hooks.register :pages, :post_render do |page|
-            page.output = output_clean(page.output)
+            page.output = Jekyll::Tidy::output_clean(page.output)
         end
     end
 end
