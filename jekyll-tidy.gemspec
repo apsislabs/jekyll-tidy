4358b3a29d00f07c7d28d8f12ca9025ac8c6b09e
Wyatt Kirby <kirby.wa@gmail.com>
2016-02-25 12:52:57 -0800

    Remove sanitize because it ruins my life

--- jekyll-tidy.gemspec
+++ jekyll-tidy.gemspec
@@ -19,7 +19,6 @@ Gem::Specification.new do |spec|
 
   spec.add_dependency "jekyll"
   spec.add_dependency "htmlbeautifier"
-  spec.add_dependency "sanitize"
 
   spec.add_development_dependency "bundler", "~> 1.6"
   spec.add_development_dependency "rake"
--- lib/jekyll/tidy.rb
+++ lib/jekyll/tidy.rb
@@ -1,20 +1,18 @@
 require "jekyll"
 require "htmlbeautifier"
-require "sanitize"
 
 module Jekyll
     module Tidy
-        def output_clean(output)
-            output = Sanitize.document(post.output, ::Sanitize::Config::RELAXED)
+        def self.output_clean(output)
             return HtmlBeautifier.beautify(output)
         end
-
-        Jekyll::Hooks.register :posts, :post_render do |post|
-            post.output = Jekyll::Tidy::output_clean(post.output)
-        end
-
-        Jekyll::Hooks.register :pages, :post_render do |page|
-            page.output = Jekyll::Tidy::output_clean(page.output)
-        end
     end
+end
+
+Jekyll::Hooks.register :posts, :post_render do |post|
+    post.output = Jekyll::Tidy::output_clean(post.output)
+end
+
+Jekyll::Hooks.register :pages, :post_render do |page|
+    page.output = Jekyll::Tidy::output_clean(page.output)
 end
