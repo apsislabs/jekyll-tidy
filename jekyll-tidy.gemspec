552827a4e3cf86654ba8d4966b023b3f6af3b7fe
Wyatt Kirby <kirby.wa@gmail.com>
2016-02-25 13:27:47 -0800

    Add option to compress HTML output completely

--- jekyll-tidy.gemspec
+++ jekyll-tidy.gemspec
@@ -19,6 +19,7 @@ Gem::Specification.new do |spec|
 
   spec.add_dependency "jekyll"
   spec.add_dependency "htmlbeautifier"
+  spec.add_dependency "htmlcompressor"
 
   spec.add_development_dependency "bundler", "~> 1.6"
   spec.add_development_dependency "rake"
--- lib/jekyll/tidy.rb
+++ lib/jekyll/tidy.rb
@@ -1,18 +1,45 @@
 require "jekyll"
 require "htmlbeautifier"
+require "htmlcompressor"
+
+JEKYLL_CONFIG = Jekyll.configuration({})
 
 module Jekyll
     module Tidy
+        def self.exclude?(path)
+            res = false
+            exclude = JEKYLL_CONFIG['jekyll_tidy'] && JEKYLL_CONFIG['jekyll_tidy']['exclude']
+
+            if exclude
+                res = exclude.to_a.include? path
+            end
+
+            return res
+        end
+
+        def self.compress_html?
+            JEKYLL_CONFIG['jekyll_tidy'] && JEKYLL_CONFIG['jekyll_tidy']['compress_html']
+        end
+
         def self.output_clean(output)
-            return HtmlBeautifier.beautify(output)
+            if compress_html?
+                compressor = HtmlCompressor::Compressor.new
+                return compressor.compress output
+            else
+                return HtmlBeautifier.beautify output
+            end
         end
     end
 end
 
 Jekyll::Hooks.register :posts, :post_render do |post|
-    post.output = Jekyll::Tidy::output_clean(post.output)
+    unless Jekyll::Tidy::exclude?(post.path)
+        post.output = Jekyll::Tidy::output_clean(post.output)
+    end
 end
 
 Jekyll::Hooks.register :pages, :post_render do |page|
-    page.output = Jekyll::Tidy::output_clean(page.output)
+    unless Jekyll::Tidy::exclude?(page.path)
+        page.output = Jekyll::Tidy::output_clean(page.output)
+    end
 end
